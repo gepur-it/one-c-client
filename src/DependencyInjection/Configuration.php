615853ab7872dfe0a9930e79acdabf1c11a30988
Andrii Yakovlev <yawa20@gmail.com>
2019-08-15 13:03:35 +0000

    fix deprecation

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -18,12 +18,12 @@ class Configuration  implements ConfigurationInterface
     /**
      * Generates the configuration tree builder.
      *
-     * @return \Symfony\Component\Config\Definition\Builder\TreeBuilder The tree builder
+     * @return TreeBuilder The tree builder
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('one_c_client');
+        $treeBuilder = new TreeBuilder('one_c_client');
+        $rootNode = $treeBuilder->getRootNode();
         $rootNode
             ->children()
                 ->scalarNode('url')
